1c4cdb04881b5169f0af13ff327888ddd0d2a035
Vitor Hiratsuka <v256589@dac.unicamp.br>
2020-09-27 16:53:16 -0300

    cria função para ler numeros sorteados e calcular acertos

--- tarefa01/megasena.c
+++ tarefa01/megasena.c
@@ -1,28 +1,59 @@
 #include <stdio.h>
-#define MAX 1000
+#define MAX 100
 
 void lerDezenas(int jogos[][6],int participantes){
-    for(int jogador=0;jogador<participantes;jogador++){
-        int posicao = 0;
-        for(int numero=1;numero<=60;numero++){
+    int jogador,posicao,marcado,numero;
+    for(jogador=0;jogador<participantes;jogador++){
+        posicao = 0;
+        for(numero=1;numero<=60;numero++){
             int marcado = getchar() == '1';
             if (marcado){
                 jogos[jogador][posicao] = numero;
-                posicao++;
+                posicao+=1;
             }
             getchar(); /*Pulando caractere de espaço*/
         }
-        if(posicao != 59) {
-            jogos[jogador][posicao+1] = 0;
+        if(posicao != 14) {
+            jogos[jogador][posicao+1] = 0; /*limite de 15 numeros no volante*/
         }
     }
 }
+void lerSorteado(int sorteado[6]){
+    for(int i=0;i<6;i++){
+        scanf("%i",sorteado[i]);
+    }
+}
+void calcularAcertos(int jogos[][15],int sorteado[6],int acertosJogadores[MAX],int jogadores){
+    int acertos=0;
+    for(int i=0;i<jogadores;i++){
+        for(int j=0;j<15;j++){
+            if(jogos[i][j]==0){
+                break;
+            }
+            for(int k=0;k<6;k++){
+                if(jogos[i][j]==sorteado[k]){
+                    acertos+=1;
+                }
+            }
+        }
+        acertosJogadores[i]=acertos;
+        acertos=0;
+    }
+}
 int main(){
-    int jogador;
-    int jogos[MAX][6];
-    int jogadores;
-    double prize;
-    scanf("&d&f", &jogadores, &prize);
+    int jogos[MAX][15],jogadores,acertosJogadores[MAX];
+    double prize,premio[MAX];
+    int sorteado[6];
+    scanf("&d &f", &jogadores, &prize);
     lerDezenas(jogos,jogadores);
+    lerSorteado(sorteado);
+    calcularAcertos(jogos,sorteado,acertosJogadores,jogadores);
+    for(int i=0;i<jogadores;i++){
+        premio[i]=0;    /*definindo premio de cada um para 0 por não saber os acertos*/
+    }
+    definirPremio(premio,acertosJogadores,jogadores,prize);
+    for(int i=0;i<jogadores;i++){
+        printf("%.2f",acertosJogadores[i]);
+    }
     return 0;
 }
