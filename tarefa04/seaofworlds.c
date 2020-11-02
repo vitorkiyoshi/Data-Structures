d9925932511ac487bec0085884ec961a13c767c2
Vitor Hiratsuka <v256589@dac.unicamp.br>
2020-11-02 13:28:32 -0300

    cria função de conferir

--- tarefa04/seaofworlds.c
+++ tarefa04/seaofworlds.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 #define MAX 21
 
+int conferir(int n, int m, char texto[m][n], int aux[m][n], char palavra[], int posicaoletra, int x, int y){
+    aux[y][x] = 1;
+    if(texto[y][x] == palavra[posicaoletra]){
+        if (palavra[posicaoletra+1] == '\00'){
+            return 1;
+        }
+        if(y-1 > -1 && !aux[y-1][x]){
+            if(conferir(n,m,texto,aux,palavra,posicaoletra+1, x, y-1)) return 1;
+        }
+        if(y+1 < m && !aux[y+1][x]){
+            if(conferir(n,m,texto,aux,palavra,posicaoletra+1, x, y+1)) return 1;
+        }
+        if(x-1 > -1 && !aux[y][x-1]){
+            if(conferir(n,m,texto,aux,palavra,posicaoletra+1, x-1, y)) return 1;
+        }
+        if(x+1 < n && !aux[y][x+1]){
+            if(conferir(n,m,texto,aux,palavra,posicaoletra+1, x+1, y)) return 1;
+        }
+    }
+    aux[y][x] = 0;
+    return 0;
+}
+
+
 int main(){
     int m, n, q;
     scanf("%i", &n);
