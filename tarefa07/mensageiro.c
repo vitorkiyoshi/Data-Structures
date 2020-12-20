27ded8aa36c7059f54cac2d1ce84974585b74a9d
Vitor Hiratsuka <v256589@dac.unicamp.br>
2020-12-20 19:54:15 -0300

    cria métodos para combinação e achar a soma correta

--- tarefa07/mensageiro.c
+++ tarefa07/mensageiro.c
@@ -35,6 +35,7 @@ p_no inserir(p_no raiz, int chave,char txt[4]){
         novo->esquerda=novo->direita==NULL;
         novo->chave = chave;
         novo->texto=txt;
+        novo->verif=0;
         return novo;
     }
     if(chave < raiz->chave){
@@ -45,21 +46,89 @@ p_no inserir(p_no raiz, int chave,char txt[4]){
     }
     return raiz;
 }
-p_no encontrarSoma(p_no arvore, int soma);
-p_no concatenar(p_no arvore1, p_no arvore2){
-    p_no concat=criarArvore(concat);
-    while(arvore1->chave!=NULL){
-        if(buscar(concat, arvore1->chave)){
-            inserir(concat, arvore1->chave, arvore1->texto);
-        }
-        arvore1=arvore1->direita;
+p_no remover_rec(p_no raiz, int chave){
+    if(raiz == NULL){
+        return NULL;
     }
-    while(arvore2->chave!=NULL){
-        if(buscar(concat, arvore2->chave)){
-            inserir(concat, arvore2->chave, arvore2->texto);
-        }
-        arvore2=arvore2->direita;
+    if(chave<raiz->chave){
+        raiz->esquerda=remover_rec(raiz->esquerda,chave);
     }
+    else if(chave>raiz->chave){
+        raiz->direita=remover_rec(raiz->direita,chave);
+    }
+    else if(raiz->esquerda==NULL){
+        return raiz->dir;
+    }
+    else if(raiz->direita==NULL){
+        return raiz->esquerda;
+    }
+    else{
+        remover_sucessor(raiz);
+    }
+    return raiz;
+}
+void remover_sucessor(p_no raiz){
+    p_no min=raiz->direita;
+    p_no pai=raiz;
+    while(min->esquerda !=NULL){
+        pai=min;
+        min=min->esquerda;
+    }
+    if(pai->esquerda==min){
+        pai->esquerda=min->direita;
+    }
+    else{
+        pai->direita=min->direita;
+    }
+    raiz->chave=min->chave;
+}
+p_no achar_dupla(p_no raiz, p_no atual, int objetivo){
+    if (atual == NULL) return NULL;
+    p_no resultado;
+    if((resultado = achar_dupla(raiz, atual->direita, objetivo))){
+        return resultado;
+    }
+    if(atual->valor < objetivo){
+        if(atual->visitado == 0) {
+            atual->visitado = 1;
+            p_no resultado = buscar_iterativo(raiz, objetivo - atual->valor);
+            if (resultado != NULL || resultado->visitado == 0) {
+                return combinar(raiz, atual, resultado);
+            }
+            atual->visitado = 0;
+        }
+        return (achar_dupla(raiz, atual->esquerda, objetivo));
+    }
+}
+
+p_no achar_tripla(p_no raiz, p_no atual, int objetivo){
+    if (atual == NULL) return 0;
+    p_no resultado;
+    if(resultado = achar_tripla(raiz, atual->direita, objetivo)) {
+        return resultado;
+    }
+    if(atual->valor < objetivo -2){
+        atual->visitado = 1;
+        if(resultado = achar_dupla(raiz, raiz, objetivo - atual->valor)){
+            return combinar(raiz, atual, resultado);
+        }
+        atual->visitado = 0;
+        return achar_tripla(raiz, atual->esquerda, objetivo);
+    }
+}
+p_no combinar(p_no raiz, p_no a, p_no b){
+    char *novo_texto = malloc(strlen(a->texto) + strlen(b->texto) + 1);
+    strcpy(novo_texto, a->texto);
+    strcat(novo_texto, b->texto);
+    p_no novo = adicionar_no(raiz,a->valor + b->valor, novo_texto);
+    remover_rec(raiz, a->valor);
+    remover_rec(raiz, b->valor);
+    destruir_no(a);
+    destruir_no(b);
+    return novo;
+}
+p_no destruirArvore(p_no arvore){
+
 }
 /*O que deve-se fazer:
  * busca binária, verificando soma dos numeros, e soma de algarismos
@@ -71,8 +140,10 @@ int main(){
     int n_cartoes,n_autoridades,i,chave,soma;
     char texto[4];
     p_no arvore;
+    p_no arvore_somas;
     p_no arvore_concatenada;
     arvore=criarArvore(arvore);
+    arvore_somas=criarArvore(arvore_somas);
     while(scanf("%i %i",&n_cartoes,&n_autoridades)!=EOF){
         for(i=0;i<n_cartoes;i++){
             scanf("%i %s",&chave,texto);
@@ -80,15 +151,19 @@ int main(){
         }
         for(i=0;i<n_autoridades;i++) {
             scanf(" %i", soma);
-            encontrarSoma(arvore,soma);
+
         }
         /*Após pegar as entradas da mensagem, botar elas na arvore
          * encontrar soma retorna por backtracking nossa soma desejada
          * se tiver mais de uma autoridade, concatenar*/
         if(n_autoridades>1){
-            arvore_concatenada=concatenar()
+
+        }
+        else{
+
         }
     }
+
 
     return 0;
 }
