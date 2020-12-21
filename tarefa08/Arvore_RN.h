ec074417d66e547cfa2ba33511a7db66a46d7a43
Vitor Hiratsuka <v256589@dac.unicamp.br>
2020-12-21 18:49:22 -0300

    cria estrutura para a arvore balanceada

---
+++ tarefa08/Arvore_RN.h
@@ -0,0 +1,24 @@
+#ifndef TAREFA08_ARVORE_RN_H
+#define TAREFA08_ARVORE_RN_H
+enum Cor {VERMELHA, PRETA};
+
+typedef struct No {
+    struct No *esquerdo, *direito, *pai;
+    enum Cor cor;
+    long valor;
+    int quantidade;
+} No;
+typedef No* p_no;
+
+p_no criar_no(long valor);
+p_no achar_pai(p_no filho);
+p_no achar_avo(p_no neto);
+p_no achar_irmao(p_no irmao);
+p_no achar_tio(p_no sobrinho);
+void rotacionar_direita(p_no no);
+void rotacionar_esquerda(p_no no);
+p_no inserir_simples(p_no raiz, p_no novo_no);
+void consertar_arvore(p_no novo);
+p_no inserir(p_no raiz, long valor_novo);
+p_no buscar(p_no raiz, long valor);
+#endif //TAREFA08_ARVORE_RN_H
