aee156c9b493e932775afcfa9a8dcf78793758b5
Vitor Hiratsuka <v256589@dac.unicamp.br>
2020-12-25 12:42:57 -0300

    reescreve heap.h

--- tarefa09/heap.h
+++ tarefa09/heap.h
@@ -6,6 +6,6 @@ typedef struct Cliente {
 typedef struct Cliente *p_cliente;
 void heapsort(p_cliente *heap, int n);//Uso de heapsort para ordenação das avaliações de cliente,
 void desce_no_heap(p_cliente *heap,int n, int k);
-void remover_elemento(p_cliente *heap, int *n);
+void remover_elemento(p_cliente *heap, int *n,char nome[15]);
 int F_ESQ(int n);
 int F_DIR(int n);
