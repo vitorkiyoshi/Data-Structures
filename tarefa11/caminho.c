4c63788ddfefa89ab470271075cd39309f34ef30
Vitor Hiratsuka <v256589@dac.unicamp.br>
2020-12-30 00:20:45 -0300

    teste algoritmo com todas as possiveis rotas

--- tarefa11/caminho.c
+++ tarefa11/caminho.c
@@ -3,10 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct ponto {//estruturas para lista de distancias
+typedef struct ponto {
     double x,y;
-    int inicial;
-    double distancia_ao_objetivo;
+    int visitado, objetivo;
 } Ponto;
 
 typedef Ponto* p_ponto;
@@ -15,13 +14,17 @@ typedef struct no {
     struct no *proximo;
 } No;
 typedef No* p_no;
-//dist. euclidiana entre dois pontos(usar estrutura de ponto)
-double calcular_distancia(p_ponto a, p_ponto b){
+double distancia(p_ponto a, p_ponto b){
     double x = a->x - b->x;
     double y = a->y - b->y;
     return sqrt(x*x + y*y);
 }
-//operações para listas
+
+typedef struct par{
+    double ordem;
+    int valor;
+} Par;
+typedef Par* p_par;
 p_no adicionar(p_no raiz, p_ponto ponto){
     p_no novo = malloc(sizeof(No));
     novo->atual = ponto;
@@ -35,10 +38,9 @@ void destruir_lista(p_no raiz){
         free(raiz);
     }
 }
-//comparador de distancias
-int comparar_pontos(const void * a, const void * b){
-    double oa = (*((p_ponto*)a))->distancia_ao_objetivo;
-    double ob = (*((p_ponto*)b))->distancia_ao_objetivo;
+int comparar_pares(const void * a, const void * b){
+    double oa = (*((p_par*)a))->ordem;
+    double ob = (*((p_par*)b))->ordem;
     if (oa > ob)
         return 1;
     else if (oa < ob)
@@ -46,76 +48,85 @@ int comparar_pontos(const void * a, const void * b){
     else
         return 0;
 }
-
-int main() {//usar então as listas para formar grafo de distancias, consistindo em achar a menor dist
+double encontrar_caminho(p_ponto *pontos, double **distancias, int atual, int tamanho, double maior_distancia){
+    pontos[atual]->visitado = 1;
+    for(int i=0;i<tamanho;i++){
+        if(pontos[i]->objetivo && distancias[atual][i] < maior_distancia){
+            maior_distancia = distancias[atual][i];
+        }
+    }
+    p_par *proximos = malloc((tamanho-2) * sizeof(p_par));
+    int quantidade_proximos = 0;
+    for(int i=0;i<tamanho;i++){
+        if(!pontos[i]->visitado && distancias[i][atual] < maior_distancia && !pontos[i]->objetivo){
+            proximos[quantidade_proximos] = malloc(sizeof(Par));
+            proximos[quantidade_proximos]->valor = i;
+            proximos[quantidade_proximos]->ordem = distancias[i][atual];
+            quantidade_proximos++;
+        }
+    }
+    qsort(proximos, quantidade_proximos, sizeof(p_par), comparar_pares);
+    for(int i=0;i<quantidade_proximos;i++){
+        if(proximos[i]->ordem<maior_distancia){
+            double distancia_caminho = encontrar_caminho(pontos,distancias, proximos[i]->valor, tamanho, maior_distancia);
+            distancia_caminho = distancia_caminho>proximos[i]->ordem?distancia_caminho:proximos[i]->ordem;
+            if(distancia_caminho<maior_distancia){
+                maior_distancia = distancia_caminho;
+            }
+        }
+    }
+    for(int i = 0; i<quantidade_proximos;i++){
+        free(proximos[i]);
+    }
+    free(proximos);
+    return maior_distancia;
+}
+int main() {
     p_no lista_pontos = NULL;
-    p_no lista_objetivos = NULL;
     int tamanho = 1;
     p_ponto leitura = malloc(sizeof(Ponto));
-    //leitura inicial
     scanf("%lf %lf", &leitura->x, &leitura->y);
-    leitura->inicial=1;
-    leitura->distancia_ao_objetivo = INFINITY;
+    leitura->visitado=1;
+    leitura->objetivo=0;
     lista_pontos = adicionar(lista_pontos, leitura);
     char tipo[15];
-    while(1){//após leitura inicial, registrar comparação entre os pontos até EOF
+    while(1){
         leitura = malloc(sizeof(Ponto));
         if(scanf("%lf %lf", &leitura->x, &leitura->y) == EOF){
             free(leitura);
             break;
         }
         scanf("%s ", tipo);
-        leitura->inicial=0;
-        if(strcmp(tipo,"Lugia")){//verificando se o tipo é lugia, superior a todos os outros no grafo
-            leitura->distancia_ao_objetivo = INFINITY;//tipologia especial que é maior que qualquer double
-            lista_pontos = adicionar(lista_pontos, leitura);
-            tamanho++;
-        } else {
-            leitura->distancia_ao_objetivo = 0;
-            lista_objetivos = adicionar(lista_objetivos, leitura);
-        }
+        leitura->visitado=0;
+        leitura->objetivo=!strcmp(tipo,"Lugia");
+        lista_pontos = adicionar(lista_pontos, leitura);
+        tamanho++;
     }
-    //usando então agora vetor para distancias, bastando calcular na hora a dist agora
     p_ponto *pontos = malloc(sizeof(p_ponto)*tamanho);
     p_no atual = lista_pontos;
     for(int i=0;i<tamanho;i++){
         pontos[i] = atual->atual;
-        p_no no_objetivo = lista_objetivos;
-        while(no_objetivo != NULL){
-            p_ponto objetivo = no_objetivo->atual;
-            double distancia_ao_objetivo = calcular_distancia(pontos[i], objetivo);
-            if(distancia_ao_objetivo < pontos[i]->distancia_ao_objetivo){
-                pontos[i]->distancia_ao_objetivo = distancia_ao_objetivo;
-            }
-            no_objetivo = no_objetivo->proximo;
-        }
         atual = atual->proximo;
     }
-    //ordenando distancias usando comparador
-    qsort(pontos, tamanho, sizeof(p_ponto), comparar_pontos);
-    for(int i=0;i<tamanho;i++){
-        for(int j=0;j<i;j++){//adicionando mais pontos de break para aumentar eficiencia
-            double distancia_i_j = calcular_distancia(pontos[i], pontos[j]);
-            if(distancia_i_j < pontos[i]->distancia_ao_objetivo){
-                if(distancia_i_j>pontos[j]->distancia_ao_objetivo) {
-                    pontos[i]->distancia_ao_objetivo = distancia_i_j;
-                } else {
-                    pontos[i]->distancia_ao_objetivo = pontos[j]->distancia_ao_objetivo;
-                    break;
-                }
-            }
-            if(pontos[j]->distancia_ao_objetivo > pontos[i]->distancia_ao_objetivo){
-                break;
-            }
-        }
-        if(pontos[i]->inicial){
-            printf("%i", (int) ceil(pontos[i]->distancia_ao_objetivo));
-            break;
+    double **distancias = malloc(tamanho * sizeof(double*));
+    for(int i = 0; i<tamanho;i++){
+        distancias[i] = malloc(tamanho*sizeof(double));
+    }
+    for(int i = 0;i<tamanho;i++){
+        distancias[i][i] = 0.0;
+        for(int j = i+1;j<tamanho;j++){
+            distancias[i][j] = distancias[j][i] = distancia(pontos[i], pontos[j]);
         }
     }
-    /*Free para: listapontos, listaobjetivos e pontos*/
-    destruir_lista(lista_pontos);
-    destruir_lista(lista_objetivos);
+    int pos_atual = tamanho-1;
+    double maior_distancia = encontrar_caminho(pontos, distancias, pos_atual, tamanho, INFINITY);
+    int maior_distancia_inteira = (int) maior_distancia;
+    printf("%i", (maior_distancia_inteira == maior_distancia? maior_distancia_inteira: maior_distancia_inteira+1));
     free(pontos);
+    for(int i = 0; i<tamanho;i++){
+        free(distancias[i]);
+    }
+    free(distancias);
+    destruir_lista(lista_pontos);
     return 0;
 }
