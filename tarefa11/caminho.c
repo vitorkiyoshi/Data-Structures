9702da127b570dad910e6b07a142bdfbfab7fa89
Vitor Hiratsuka <v256589@dac.unicamp.br>
2020-12-30 01:07:07 -0300

    retira arquivo para correção e comenta código refeito

--- tarefa11/caminho.c
+++ tarefa11/caminho.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
-
+//Estruturas para as listas de pontos
 typedef struct ponto {
     double x,y;
     int visitado, objetivo;
@@ -20,11 +20,12 @@ double distancia(p_ponto a, p_ponto b){
     return sqrt(x*x + y*y);
 }
 
-typedef struct par{
+typedef struct par{//agora uso de pares para indicar a distancia e delas pegar a menor possivel
     double ordem;
     int valor;
 } Par;
 typedef Par* p_par;
+//operações para listas
 p_no adicionar(p_no raiz, p_ponto ponto){
     p_no novo = malloc(sizeof(No));
     novo->atual = ponto;
@@ -38,7 +39,7 @@ void destruir_lista(p_no raiz){
         free(raiz);
     }
 }
-int comparar_pares(const void * a, const void * b){
+int comparar_pares(const void * a, const void * b){//compara a de menor distancia e utiliza em qsort
     double oa = (*((p_par*)a))->ordem;
     double ob = (*((p_par*)b))->ordem;
     if (oa > ob)
@@ -48,6 +49,7 @@ int comparar_pares(const void * a, const void * b){
     else
         return 0;
 }
+//encontra caminho, percorrendo da origem cada percurso possível e denomina o menor
 double encontrar_caminho(p_ponto *pontos, double **distancias, int atual, int tamanho, double maior_distancia){
     pontos[atual]->visitado = 1;
     for(int i=0;i<tamanho;i++){
@@ -66,7 +68,7 @@ double encontrar_caminho(p_ponto *pontos, double **distancias, int atual, int ta
         }
     }
     qsort(proximos, quantidade_proximos, sizeof(p_par), comparar_pares);
-    for(int i=0;i<quantidade_proximos;i++){
+    for(int i=0;i<quantidade_proximos;i++){//achando a menor distancia
         if(proximos[i]->ordem<maior_distancia){
             double distancia_caminho = encontrar_caminho(pontos,distancias, proximos[i]->valor, tamanho, maior_distancia);
             distancia_caminho = distancia_caminho>proximos[i]->ordem?distancia_caminho:proximos[i]->ordem;
@@ -85,12 +87,12 @@ int main() {
     p_no lista_pontos = NULL;
     int tamanho = 1;
     p_ponto leitura = malloc(sizeof(Ponto));
-    scanf("%lf %lf", &leitura->x, &leitura->y);
+    scanf("%lf %lf", &leitura->x, &leitura->y);//leitura inicial
     leitura->visitado=1;
     leitura->objetivo=0;
     lista_pontos = adicionar(lista_pontos, leitura);
     char tipo[15];
-    while(1){
+    while(1){//popular a lista com o objetivo
         leitura = malloc(sizeof(Ponto));
         if(scanf("%lf %lf", &leitura->x, &leitura->y) == EOF){
             free(leitura);
@@ -98,30 +100,32 @@ int main() {
         }
         scanf("%s ", tipo);
         leitura->visitado=0;
-        leitura->objetivo=!strcmp(tipo,"Lugia");
+        leitura->objetivo=!strcmp(tipo,"Lugia");//verificando caso seja lugia
         lista_pontos = adicionar(lista_pontos, leitura);
         tamanho++;
     }
-    p_ponto *pontos = malloc(sizeof(p_ponto)*tamanho);
+    p_ponto *pontos = malloc(sizeof(p_ponto)*tamanho);//variavel para vetor de pontos
     p_no atual = lista_pontos;
     for(int i=0;i<tamanho;i++){
         pontos[i] = atual->atual;
         atual = atual->proximo;
     }
-    double **distancias = malloc(tamanho * sizeof(double*));
+    double **distancias = malloc(tamanho * sizeof(double*));//calculo de distancias usando uma matriz
     for(int i = 0; i<tamanho;i++){
         distancias[i] = malloc(tamanho*sizeof(double));
     }
     for(int i = 0;i<tamanho;i++){
         distancias[i][i] = 0.0;
         for(int j = i+1;j<tamanho;j++){
-            distancias[i][j] = distancias[j][i] = distancia(pontos[i], pontos[j]);
+            distancias[i][j] = distancias[j][i] = distancia(pontos[i], pontos[j]);//achando distancias para colocá-las na matriz
         }
     }
+    //encontrando melhor caminho de todos os percursos
     int pos_atual = tamanho-1;
     double maior_distancia = encontrar_caminho(pontos, distancias, pos_atual, tamanho, INFINITY);
     int maior_distancia_inteira = (int) maior_distancia;
     printf("%i", (maior_distancia_inteira == maior_distancia? maior_distancia_inteira: maior_distancia_inteira+1));
+    /*free para: pontos, distancias(tamanho), lista de pontos*/
     free(pontos);
     for(int i = 0; i<tamanho;i++){
         free(distancias[i]);
