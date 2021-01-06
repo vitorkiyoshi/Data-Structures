9410a7e0b17115eb71d9e53b6802829204791ad1
Vitor Hiratsuka <v256589@dac.unicamp.br>
2021-01-06 01:50:11 -0300

    encapsula structs

--- tarefa12/planilhas.c
+++ tarefa12/planilhas.c
@@ -4,20 +4,6 @@
 #define MAX_CELULA 7
 #define MAX_CAMINHO 30
 #define MAX_STRING 1000
-typedef struct lista_token{
-    char *token;
-    struct lista_token *proximo;
-} Lista_token;
-
-typedef Lista_token* p_lista;
-
-typedef struct celula{
-    char **expressao;
-    int valor;
-    int calculado;
-} Celula;
-typedef Celula* p_celula;
-
 p_lista adicionar(p_lista raiz, char *token) {//adiciona token na lista de tokens
     p_lista novo = malloc(sizeof(Lista_token));
     novo->token=token;
